50b2f63998c0a74f986c0204ef34b004094451fe
arichrath13 <anna-lena.richrath@web.de>
2023-10-17 12:50:43 +0200

    Update api.py
    added blueprint

--- api/api.py
+++ api/api.py
@@ -1,6 +1,10 @@
 import flask
 from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
+from api.routes.client_api import client_blueprint
+from api.routes.kellner_api import kellner_blueprint
+
+DATABASE = 'C:\Users\fclen\Documents\Berufsschule\FU1\tischreservierung-backer_richrath\api\schema.sql'
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
@@ -8,5 +12,8 @@ app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur ei
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Tischreservierung</h1>"
+
+app.register_blueprint(client_blueprint, url_prefix='/client')
+app.register_blueprint(kellner_blueprint, url_prefix='/kellner')
 
 app.run()
